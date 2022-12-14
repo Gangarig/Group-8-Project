a70b4ebd6e778f43b7288ff8c413f8dde0cbb767
Ivica Acic <ivicaacic@gmail.com>
2022-12-14 14:42:44 +0100

    update gallery

--- index.php
+++ index.php
@@ -19,6 +19,7 @@ if (isset($_SESSION["status"])) {
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css" />
   <link rel="stylesheet" href="style/style.css" />
+  
   <title>First Aid Courses</title>
 </head>
 
