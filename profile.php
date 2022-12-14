2fdb4d308c348a301c88985f859487a91a1d4ed8
kristina trifunovic <kristinat306@gmail.com>
2022-12-14 11:29:13 +0100

    improve profile page

--- profile.php
+++ profile.php
@@ -45,13 +45,16 @@ $row = mysqli_fetch_array($res, MYSQLI_ASSOC);
                 <div class="col-3">
                     <img style="height: 250px; width: 100% !important;" align="right" src="./actions/images/<?php echo $row['profile_img'] ?>" class="img-fluid w-25" alt="profile.img">
                 </div>
-                <div class="col-9">
-                    <p style="font-family: Roboto Condensed;">Name : <?php echo $row['fname'] . ' ' . $row['lname'] ?></p>
-                    <p style="font-family: Roboto Condensed;">Birth Date : <?php echo $row['birth_date'] ?></p>
-                    <p style="font-family: Roboto Condensed;">Email : <?php echo $row['email'] ?></p>
-                    <p style="font-family: Roboto Condensed;">Address : <?php echo $row['address'] ?></p>
-                    <p style="font-family: Roboto Condensed;">Phone Number : <?php echo $row['phone_number'] ?></p>
-                    <p style="font-family: Roboto Condensed;">Profile status : <?php echo $row['status'] ?></p>
+                <div class="col-1"></div>
+                <div class="col-4" style="margin-top: 30px;">
+                    <p style="font-family: Roboto Condensed;font-size: 20px;">Name : <?php echo $row['fname'] . ' ' . $row['lname'] ?></p>
+                    <p style="font-family: Roboto Condensed;font-size: 20px;">Birth Date : <?php echo $row['birth_date'] ?></p>
+                    <p style="font-family: Roboto Condensed;font-size: 20px;">Email : <?php echo $row['email'] ?></p>
+                </div>
+                <div class="col-4" style="margin-top: 30px;>
+                    <p style="font-family: Roboto Condensed;font-size: 20px;">Address : <?php echo $row['address'] ?></p>
+                    <p style="font-family: Roboto Condensed;font-size: 20px;">Phone Number : <?php echo $row['phone_number'] ?></p>
+                    <p style="font-family: Roboto Condensed;font-size: 20px;">Profile status : <?php echo $row['status'] ?></p>
                     <a class="btn btn-dark" href="actions/login/Edit.php?id=<?php echo $_SESSION['user'] ?>">Edit Profile</a>
                 </div>
             </div>
