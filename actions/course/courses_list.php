0ed65c4b15e270089ab325f08d035a0f249b0b3e
Chrisyur <chris-yurt@hotmail.com>
2022-12-15 02:04:10 +0100

    restructured courses_list page

--- actions/course/courses_list.php
+++ actions/course/courses_list.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 session_start();
 require_once('../components/boot.php');
 require_once('../components/db_connect.php');
@@ -19,88 +19,95 @@ if (isset($_SESSION['trainer'])) {
 }
 
 $result = mysqli_query($link, "SELECT * FROM courses");
-$list ='';
+$list = '';
+
 while ($row = mysqli_fetch_array($result)) {
 
-
-$list .="
-  <table class='table m-5 table-striped'>
-
-    <tr>
-        <th>ID</th>
-        <td class='col-4'>".$row['id']." </td>
-    </tr>
-    <tr>
-        <th>Cours name</th>
-        <td class='col-4'>".$row['name']."</td>
-    </tr>
-    <tr>
-        <th>Duration</th>
-        <td class='col-4'>".$row['duration']." </td>
-    </tr>
-    <tr>
-        <th>Address</th>
-        <td class='col-4'>".$row['address']." </td>
-    </tr>
-    <tr>
-        <th>Start date</th>
-        <td class='col-4'>".$row['start_date']." </td>
-    </tr>
-    <tr>
-        <th>End date</th>
-        <td class='col-4'>".$row['end_date']." </td>
-    </tr>
-    <tr>
-        <th>Price private</th>
-        <td class='col-4'>".$row['price_private']." </td>
-    </tr>
-    <tr>
-        <th>Price business</th>
-        <td class='col-4'>".$row['price_business']." </td>
-    </tr>
-    <tr>
-        <th>Price student</th>
-        <td class='col-4'>".$row['price_student']." </td>
-    </tr>
-    <tr>
-        <th>Fk trainer</th>
-        <td class='col-4'>".$row['fk_trainer']." </td>
-    </tr>
-    <tr>
-        <th>Capacity</th>
-        <td class='col-4'>".$row['capacity']." </td>
-    </tr>
-    <tr>
-        <th>Image</th>
-        <td class='col-4'>".$row['image']." </td>
-    </tr>
-    <tr>
-        <th>Description</th>
-        <td class='col-4'>".$row['description']." </td>
-        <td><a class='btn btn-dark' href='delete_course.php?id=".$row['id']."' >Delete</a></td>
-        <td><a class='btn btn-dark' href='update_courses.php?id=".$row['id']."' >Update</a></td>
-
-
-    </tr>
-  </table>
-
-";}
-
-
+    $list .= "
+<table class='table table-striped table-borderless w-75 mx-auto my-5'>
+    <tbody>
+        <tr>
+            <th>ID</th>
+            <td>" . $row['id'] . " </td>
+        </tr>
+        <tr>
+            <th>Course name</th>
+            <td>" . $row['name'] . "</td>
+        </tr>
+        <tr>
+            <th>Duration</th>
+            <td>" . $row['duration'] . " </td>
+        </tr>
+        <tr>
+            <th>Address</th>
+            <td>" . $row['address'] . " </td>
+        </tr>
+        <tr>
+            <th>Start date</th>
+            <td>" . $row['start_date'] . " </td>
+        </tr>
+        <tr>
+            <th>End date</th>
+            <td>" . $row['end_date'] . " </td>
+        </tr>
+        <tr>
+            <th>Price private</th>
+            <td>" . $row['price_private'] . " </td>
+        </tr>
+        <tr>
+            <th>Price business</th>
+            <td>" . $row['price_business'] . " </td>
+        </tr>
+        <tr>
+            <th>Price student</th>
+            <td>" . $row['price_student'] . " </td>
+        </tr>
+        <tr>
+            <th>Trainer ID</th>
+            <td>" . $row['fk_trainer'] . " </td>
+        </tr>
+        <tr>
+            <th>Capacity</th>
+            <td>" . $row['capacity'] . " </td>
+        </tr>
+        <tr>
+            <th>Image</th>
+            <td>" . $row['image'] . " </td>
+        </tr>
+        <tr>
+            <th>Description</th>
+            <td>" . $row['description'] . " </td>
+        </tr>
+        <tr>
+            <td>
+                <a class='btn btn-dark' href='update_courses.php?id=" . $row['id'] . "' >Update</a>
+            </td>
+            <td>
+                <a class='btn btn-dark' href='delete_course.php?id=" . $row['id'] . "' >Delete</a>
+            </td>
+        </tr>
+    </tbody>
+</table>";
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <title>Registered Accounts</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/style.css" rel="stylesheet">
-    </head>
-    <body>
-    <?php echo $list; ?>
-    <div class="w-100 text-center">
-    <td><a class='btn btn-dark' href='create_courses.php?id=".$row['id']."' >Create new course</a></td>
-    <a class=" btn btn-dark" href="../../dashboard.php">Back</a>
+
+<head>
+    <title>Courses List</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+    <div class="container-fluid">
+        <?php echo $list; ?>
     </div>
-    </body>
+    <div class="w-100 text-center my-4">
+        <td><a class='btn btn-dark' href='create_courses.php'>Create new course</a></td>
+        <a class="btn btn-dark" href="../../dashboard.php">Back</a>
+    </div>
+</body>
+
 </html>
