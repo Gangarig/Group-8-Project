648734c7671d9f6d1e4724898e48084314448d20
Chrisyur <chris-yurt@hotmail.com>
2022-12-15 11:09:04 +0100

    restructured and changed edit.php format

--- actions/login/Edit.php
+++ actions/login/Edit.php
@@ -12,7 +12,7 @@ if (!isset($_SESSION['admin']) && !isset($_SESSION['user']) && !isset($_SESSION[
 $backBtn = '';
 //if it is a user it will create a back button to home.php
 if (isset($_SESSION["user"])) {
-    $backBtn = "../../profile.php";
+    $backBtn = "../../profile.php#profile";
 }
 //if it is a admin it will create a back button to dashboard.php
 if (isset($_SESSION["admin"])) {
@@ -32,7 +32,6 @@ if (isset($_GET['id'])) {
         $profile_img = $data['profile_img'];
         $phone_number = $data['phone_number'];
         $address = $data['address'];
-
     }
 }
 //update
@@ -66,20 +65,17 @@ if (isset($_POST["submit"])) {
         WHERE id = {$id}";
     }
     if (mysqli_query($connect, $sql) === true) {
-        $class = "alert alert-dark";
-        $message = "The record was successfully updated";
+        $class = "alert alert-success";
+        $message = "User data was successfully updated";
         $uploadError = ($pictureArray->error != 0) ? $pictureArray->ErrorMessage : '';
         header("refresh:3;url=edit.php?id={$id}");
     } else {
         $class = "alert alert-dark";
-        $message = "Error while updating record : <br>" . $connect->error;
+        $message = "Error while updating user data : <br>" . $connect->error;
         $uploadError = ($pictureArray->error != 0) ? $pictureArray->ErrorMessage : '';
         header("refresh:3;url=edit.php?id={$id}");
     }
 }
-
-
-
 mysqli_close($connect);
 
 ?>
@@ -92,31 +88,30 @@ mysqli_close($connect);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit Profile Info</title>
 </head>
-<body>
 
-<div class="<?php echo $class; ?>" role="alert">
+<body>
+    <div class="mt-5 d-flex justify-content-center">
+        <img src='pictures/<?php echo $picture ?>' alt="">
+    </div>
+    <div class="container mt-5 w-50">
+        <form class="" method="post" autocomplete="off" enctype="multipart/form-data">
+            <hr />
+            <input type="text" name="fname" class="form-control m-1" maxlength="50" value="<?php echo $fname ?>" />
+            <input type="text" name="lname" class="form-control m-1" maxlength="50" value="<?php echo $lname ?>" />
+            <input type="text" name="address" class="form-control m-1" value="<?php echo $address ?>" /> <span class="text-danger">
+                <input class='m-1 form-control ' type="file" name="picture">
+                <input class='m-1 form-control' type="text" name="phone_number" placeholder="size" maxlength="255" value="<?php echo $phone_number; ?>">
+                <hr />
+                <input type="hidden" name="profile_img" value="<?php echo $profile_img ?>" />
+                <input type="hidden" name="id" value="<?php echo $data['id'] ?>" />
+                <button type="submit" class="btn btn-block btn-dark" name="submit">Save the changes</button>
+                <a href="<?php echo $backBtn ?>"><button class="btn btn-dark" type="button">Back</button></a>
+        </form>
+        <div class="mt-5 <?php echo $class; ?>" role="alert">
             <p><?php echo ($message) ?? ''; ?></p>
             <p><?php echo ($uploadError) ?? ''; ?></p>
-</div>
-<div class="mt-5 d-flex justify-content-center">
-<img src='pictures/<?php echo $picture?>' alt="">
-</div>
-<div class="container mt-5">
-        <form class="w-75" method="post"  autocomplete="off" enctype="multipart/form-data">
-        <hr />
-        <input type="text" name="fname" class="form-control m-1" maxlength="50" value="<?php echo $fname ?>" />
-        <input type="text" name="lname" class="form-control m-1" maxlength="50" value="<?php echo $lname ?>" />
-        <input type="text" name="address" class="form-control m-1"  value="<?php echo $address ?>" /> <span class="text-danger">
-        <input class='m-1 form-control ' type="file" name="picture">
-        <input class='m-1 form-control' type="text" name="phone_number" placeholder="size" maxlength="255" value="<?php echo $phone_number; ?>"> 
-        <hr />
-        <input type="hidden" name="profile_img" value="<?php echo $profile_img ?>" />
-        <input type="hidden" name="id" value="<?php echo $data['id'] ?>" />
-        <button type="submit" class="btn btn-block btn-dark" name="submit">save the changes</button>
-        <a href="<?php echo $backBtn ?>"><button class="btn btn-dark" type="button">Back</button></a>
-        </form>
-</div>
-       
-
+        </div>
+    </div>
 </body>
+
 </html>
